db7ea923edbb6267b3f8ed5b96577fd9ac4ebb0e
OliwiaSzewczyk <oszewczyk@student.agh.edu.pl>
2025-04-28 13:32:14 +0200

    mouse

--- rtl/top_vga.sv
+++ rtl/top_vga.sv
@@ -22,7 +22,9 @@ module top_vga (
         output logic hs,
         output logic [3:0] r,
         output logic [3:0] g,
-        output logic [3:0] b
+        output logic [3:0] b,
+        inout ps2_clk,
+        inout ps2_data
 
     );
 
@@ -36,6 +38,7 @@ module top_vga (
 
     timing_if timing_if();
     vga_if background_if();
+    vga_if draw_mouse();
 
 
     /**
@@ -44,7 +47,12 @@ module top_vga (
 
     import vga_pkg::*;
 
-    
+    logic [11:0]xpos;
+    logic [11:0]ypos;
+    logic [11:0]xpos_bf1;
+    logic [11:0]ypos_bf1;
+    logic [11:0]xpos_bf2;
+    logic [11:0]ypos_bf2;
     
     /**
      * Signals assignments
@@ -73,6 +81,54 @@ module top_vga (
         .in(timing_if),
         .out(background_if)
     );
+
+    MouseCtl u_MouseCtl (
+        .ps2_clk (ps2_clk),
+        .ps2_data (ps2_data),
+        .xpos (xpos),
+        .ypos (ypos),
+        .clk (clk100),
+        .rst (rst),
+        .value(12'b0),
+        .setx('b0),
+        .sety('b0),
+        .setmax_x('b0),
+        .setmax_y('b0),
+        .new_event(),
+        .zpos(),
+        .left(),
+        .middle(),
+        .right()
+    );
+
+    buffor1 u_buffor1(
+        .clk(clk65),
+        .rst(rst),
+        .xpos(xpos),
+        .ypos(ypos),
+        .xpos_bf1(xpos_bf1),
+        .ypos_bf1(ypos_bf1)
+    );
+
+    buffor2 u_buffor2(
+        .clk(clk65),
+        .rst(rst),
+        .xpos_bf1(xpos_bf1),
+        .ypos_bf1(ypos_bf1),
+        .xpos_bf2(xpos_bf2),
+        .ypos_bf2(ypos_bf2)
+    );
+
+    draw_mouse u_draw_mouse(
+        .rst,   
+        .clk(clk65),
+        .xpos(xpos_bf2),
+        .ypos(ypos_bf2),
+        
+        .in(background_if),
+        .out(draw_mouse)
+        
+        );
 
  
 endmodule
